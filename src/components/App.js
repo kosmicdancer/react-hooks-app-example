6d514657521853fe1f741a14367952c24b98a787
Kyoko <kyokoconnect@gmail.com>
2021-05-15 01:40:42 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
 import React from "react";
-import { format } from "date-fns";
+// import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
-// import TestComponent from "./TestComponent";
+import TestComponent from "./TestComponent";
 
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      <header>
+      <h1>"Now"</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
         HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
-      <ExampleComponent />
+      </header>
+      
+    {/* <div className="example">
+        <img
+          src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+          alt="a deeply impressed man, perhaps thinking 'whoa'"
+        />
+      <p>Whoa!</p>
+    </div>
+    <div className="video">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/5TbUxGZtwGI"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        title="time video"
+        allowFullScreen
+      ></iframe>
+    </div> */}
+      <ExampleComponent />  <TestComponent /> 
     </div>
   );
 }
